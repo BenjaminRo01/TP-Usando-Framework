d3ec58dc9cdd2862628a98ed3bc23c8d87ffbe2c
BenjaminRo01 <benjaroselo01@gmail.com>
2024-06-16 00:33:15 -0300

    Utilizando el framework

---
+++ src/main/java/roselo/main/Main.java
@@ -0,0 +1,14 @@
+package roselo.main;
+
+import roselo.framework.Start;
+import roselo.model.AccionDos;
+import roselo.model.AccionUno;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        var start = new Start(List.of(new AccionUno(), new AccionDos()));
+        start.init();
+    }
+}
